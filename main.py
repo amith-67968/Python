1e4bcc78dc11871ce08ee495faef3f5adbfd67e4
amith-67968 <amithbhat128@gmail.com>
2026-04-03 11:38:43 +0530

    Add print statements for greeting and additional output

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+print("hello \n\"How\" are you?") 
+# hiiiiikhvjbfsdsffiufsbbwlfbsjbsdlf
+# bfdkjdbsdskfbkfbscfjhfiucasslifsdsffeffdf
+# ffvkfvefhjf
+
+
+print("hey",6,7,sep="-",end="009")
 print("hello")
