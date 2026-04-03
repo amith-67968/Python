66511bb84f8019f3a1f1d6c05bb0bb719c802959
amith-67968 <amithbhat128@gmail.com>
2026-04-03 12:20:59 +0530

    Add variable assignments and print statements for demonstration

---
+++ variable.py
@@ -0,0 +1,14 @@
+a=1234567890
+print(a)
+amith=20
+b=amith
+print(b)
+print("Type of a is",type(a))
+c=complex(5,8)
+print(c)
+list=['a',1,2,3,4,'hi',['hi','hello']]
+print(list)
+tuple=(('hi','hello'),('apple','banana'))
+print(tuple)
+dict={"name":"amith","age":20,"canVote":True}
+print(dict)
