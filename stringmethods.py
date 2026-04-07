970735152b1eefd5d8dc0e6c4c372d485bc1d933
amith-67968 <amithbhat128@gmail.com>
2026-04-07 19:36:38 +0530

    Add examples demonstrating string methods including upper, lower, lstrip, replace, split, and capitalize with print statements

---
+++ stringmethods.py
@@ -0,0 +1,9 @@
+a="!!!!amith !!!!! amith"
+print(len(a))
+print(a.upper())
+print(a.lower())
+print(a.lstrip("!"))
+print(a.replace("amith","john"))
+print(a.split(" "))
+blogHeading="introduction to js"
+print(blogHeading.capitalize())
