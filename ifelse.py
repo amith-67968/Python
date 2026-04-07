dd0139e12daf40f22c5b2849463e136d9b1aeb5e
amith-67968 <amithbhat128@gmail.com>
2026-04-07 23:58:16 +0530

    Remove age-related examples and retain apple budget logic

--- ifelse.py
+++ ifelse.py
@@ -1,9 +1,19 @@
-age=int(input("Enter your age:"))
-print(age>13)
-print(age<13)
-print(age==18)
-print(age!=13)
-if(age>18):
-    print("You can drive")
+# age=int(input("Enter your age:"))
+# print(age>13)
+# print(age<13)
+# print(age==18)
+# print(age!=13)
+# if(age>18):
+#     print("You can drive")
+# else:
+#     print("You can't drive")
+applePrice=100
+budget=500
+if(budget-applePrice>250):
+    print("Buy 3kg apples")
+elif(budget-applePrice>150):
+    print("Buy 2kg apples")
+elif(budget-applePrice>50):
+    print("Buy 1kg apples")
 else:
-    print("You can't drive")
+    print("Don't buy the apples")
