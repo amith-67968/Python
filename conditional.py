2b191a593178f39e203def3857f923c51736915f
amith-67968 <amithbhat128@gmail.com>
2026-04-13 00:43:27 +0530

    Refactor comments in conditional.py to maintain structure for future implementation

--- conditional.py
+++ conditional.py
@@ -1,11 +1,16 @@
-# x=int(input("Enter the value of x:"))
-# match x:
-#     case 0:
-#         print("X is zero")
-#     case 4:
-#         print("Case is 4")
-#     case _:
-#         print("case is _")
+# # x=int(input("Enter the value of x:"))
+# # match x:
+# #     case 0:
+# #         print("X is zero")
+# #     case 4:
+# #         print("Case is 4")
+# #     case _:
+# #         print("case is _")
 
-for i in range(1,100):
-    print(i)
+# for i in range(1,100):
+#     print(i)
+colors=["Red","Blue","Green"]
+for color in colors:
+    print(color)
+    for i in color:
+        print(i)
