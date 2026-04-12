bf404b7f9b282dbd8696c5eae24d7317125b3ddc
amith-67968 <amithbhat128@gmail.com>
2026-04-13 00:56:06 +0530

    while loop again

--- conditional.py
+++ conditional.py
@@ -1,23 +1,27 @@
-# x=int(input("Enter the value of x:"))
-# match x:
-#     case 0:
-#         print("X is zero")
-#     case 4:
-#         print("Case is 4")
-#     case _:
-#         print("case is _")
+# # x=int(input("Enter the value of x:"))
+# # match x:
+# #     case 0:
+# #         print("X is zero")
+# #     case 4:
+# #         print("Case is 4")
+# #     case _:
+# #         print("case is _")
 
-# for i in range(1,100):
-#     print(i)
-# colors=["Red","Blue","Green"]
-# for color in colors:
-#     print(color)
-#     for i in color:
-#         print(i)
+# # for i in range(1,100):
+# #     print(i)
+# # colors=["Red","Blue","Green"]
+# # for color in colors:
+# #     print(color)
+# #     for i in color:
+# #         print(i)
 
-# for i in range(1,20,4):
+# # for i in range(1,20,4):
+# #     print(i)
+# i=0
+# while(i<10):
 #     print(i)
-i=0
-while(i<10):
-    print(i)
-    i=i+1
+#     i=i+1
+count=6
+while(count>0):
+    print(count)
+    count=count-1
