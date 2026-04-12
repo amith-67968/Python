e576bc647bf026522e2d8e27ccaae6c44c2293e6
amith-67968 <amithbhat128@gmail.com>
2026-04-13 00:41:59 +0530

    Comment out match-case structure and retain loop for printing numbers

--- conditional.py
+++ conditional.py
@@ -1,8 +1,11 @@
-x=int(input("Enter the value of x:"))
-match x:
-    case 0:
-        print("X is zero")
-    case 4:
-        print("Case is 4")
-    case _:
-        print("case is _")
+# x=int(input("Enter the value of x:"))
+# match x:
+#     case 0:
+#         print("X is zero")
+#     case 4:
+#         print("Case is 4")
+#     case _:
+#         print("case is _")
+
+for i in range(1,100):
+    print(i)
