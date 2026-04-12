742231348ee02a648362d29664d1ddc10ce7ed66
amith-67968 <amithbhat128@gmail.com>
2026-04-13 01:08:04 +0530

    comment

--- conditional.py
+++ conditional.py
@@ -1,26 +1,26 @@
-# # x=int(input("Enter the value of x:"))
-# # match x:
-# #     case 0:
-# #         print("X is zero")
-# #     case 4:
-# #         print("Case is 4")
-# #     case _:
-# #         print("case is _")
+x=int(input("Enter the value of x:"))
+match x:
+    case 0:
+        print("X is zero")
+    case 4:
+        print("Case is 4")
+    case _:
+        print("case is _")
 
-# # for i in range(1,100):
-# #     print(i)
-# # colors=["Red","Blue","Green"]
-# # for color in colors:
-# #     print(color)
-# #     for i in color:
-# #         print(i)
+for i in range(1,100):
+    print(i)
+colors=["Red","Blue","Green"]
+for color in colors:
+    print(color)
+    for i in color:
+        print(i)
 
-# # for i in range(1,20,4):
-# #     print(i)
-# i=0
-# while(i<10):
-#     print(i)
-#     i=i+1
+for i in range(1,20,4):
+    print(i)
+i=0
+while(i<10):
+    print(i)
+    i=i+1
 count=6
 while(count>0):
     print(count)
