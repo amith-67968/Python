e0b2f12c5b32ee7bff0ddbc90a98c64f807a6485
amith-67968 <amithbhat128@gmail.com>
2026-04-26 20:05:37 +0530

    Refactor code to comment out unused variables and improve function structure for mean calculation

--- functions.py
+++ functions.py
@@ -1,4 +1,16 @@
-a=10
-b=20
-gmean=(a*b)/(a+b)
-print(gmean)
+# a=10
+# b=20
+# gmean=(a*b)/(a+b)
+# print(gmean)
+
+
+def calculate(a,b):
+    mean=(a*b)/(a+b)
+    print(mean)
+
+a=5
+b=6
+c=9
+d=2
+calculate(a,b)
+calculate(c,d)
