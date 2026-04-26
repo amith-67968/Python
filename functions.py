343efa8383568771fc74d5b98284af722d56ba32
amith-67968 <amithbhat128@gmail.com>
2026-04-26 23:11:39 +0530

    Comment out unused code and functions for clarity

--- functions.py
+++ functions.py
@@ -1,19 +1,24 @@
-# a=10
-# b=20
-# gmean=(a*b)/(a+b)
-# print(gmean)
+# # a=10
+# # b=20
+# # gmean=(a*b)/(a+b)
+# # print(gmean)
 
 
-def calculate(a,b):
-    mean=(a*b)/(a+b)
-    print(mean)
+# def calculate(a,b):
+#     mean=(a*b)/(a+b)
+#     print(mean)
 
-a=5
-b=6
-c=9
-d=2
-calculate(a,b)
-calculate(c,d)
+# a=5
+# b=6
+# c=9
+# d=2
+# calculate(a,b)
+# calculate(c,d)
 
-def calci(a,b):
-    pass
+# def calci(a,b):
+#     pass
+
+def average(a,b):
+    print("The average is ",(a+b)/2)
+
+average(4,6)
