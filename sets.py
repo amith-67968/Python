d7e1e3b6feed40aadbb0e3912e13579e7b47b177
amith-67968 <amithbhat128@gmail.com>
2026-05-07 22:41:42 +0530

    Comment out initial set examples and add union operation demonstration

--- sets.py
+++ sets.py
@@ -1,10 +1,17 @@
-s={2,4,2,6}
-print(s)
-info={"Amith",19,True,5.6,19}
-print(info)
-amith=set()
-print(type(amith))
+# s={2,4,2,6}
+# print(s)
+# info={"Amith",19,True,5.6,19}
+# print(info)
+# amith=set()
+# print(type(amith))
 
 
-for i in info:
-    print(i)
+# for i in info:
+#     print(i)
+
+s1={1,2,5,6}
+s2={3,6,7}
+
+print(s1.union(s2))
+s1.update(s2)
+print(s1,s2)
