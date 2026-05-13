2d8e6963b37c2aaf5d29766c45b0c348fc5330e6
amith-67968 <amithbhat128@gmail.com>
2026-05-13 23:56:05 +0530

    custom exception

--- exception.py
+++ exception.py
@@ -1,10 +1,15 @@
-a=input("Enter the number:")
-print(f"Multiplication table of {a} is:")
+# a=input("Enter the number:")
+# print(f"Multiplication table of {a} is:")
 
-try:
-    for i in range(1,11):
-        print(f"{a}X {i}={int(a)*i}")
-except Exception as e:
-    print(e)
-finally:
-    print("end of program")
+# try:
+#     for i in range(1,11):
+#         print(f"{a}X {i}={int(a)*i}")
+# except Exception as e:
+#     print(e)
+# finally:
+#     print("end of program")
+
+a=int(input("Enter any value between 5 and 9:"))
+
+if(a<5 or a>9):
+    raise ValueError("Value should be between 5 and 9")
