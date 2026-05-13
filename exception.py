83f115b2be08554baeb311d9ff47de765a258192
amith-67968 <amithbhat128@gmail.com>
2026-05-13 23:21:28 +0530

    exception

--- exception.py
+++ exception.py
@@ -4,7 +4,7 @@ print(f"Multiplication table of {a} is:")
 try:
     for i in range(1,11):
         print(f"{a}X {i}={int(a)*i}")
-except:
-    print("error")
+except Exception as e:
+    print(e)
 finally:
     print("end of program")
