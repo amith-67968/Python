15e2b81c55dba4f1cc5b935f10722221f2cbaf8f
amith-67968 <amithbhat128@gmail.com>
2026-05-21 23:51:54 +0530

    os

--- os.py
+++ os.py
@@ -1,7 +1,14 @@
 import os
 
-if(not os.path.exists("data")):
-    os.mkdir("data")
+# if(not os.path.exists("data")):
+#     os.mkdir("data")
 
-for i in range(1,100):
-    os.mkdir(f"data/Day{i}")
+# for i in range(1,100):
+#     os.mkdir(f"data/Day{i}")
+
+# for i in range(1,100):
+#     os.rename(f"data/Tutorial{i}",f"data/Tutorial {i}")
+
+
+folders=os.listdir()
+print(folders)
