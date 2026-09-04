67f68a7853f00475988e806080e2ed14e55c566d
amith-67968 <amithbhat128@gmail.com>
2026-09-04 11:22:38 +0530

    refactor: comment out unused file operations and maintain read functionality

--- fileio.py
+++ fileio.py
@@ -1,8 +1,15 @@
-# f=open('myfile2.txt','w')
+# # f=open('myfile2.txt','w')
 
-# text=f.write("hiii")
-# print(f)
-# f.close()
+# # text=f.write("hiii")
+# # print(f)
+# # f.close()
 
-with open('myfile.txt','a') as  f:
-    f.write("heyy")
+# with open('myfile.txt','a') as  f:
+#     f.write("heyy")
+
+f=open('myfile.txt','r')
+while True:
+    line=f.readline()
+    print(line,type(line))
+    if not line:
+        break
