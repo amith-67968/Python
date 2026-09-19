f0f97e9154a8fe67d8f781b2f21f20194ea9fa59
amith-67968 <amithbhat128@gmail.com>
2026-09-19 16:39:31 +0530

    truncate

--- seek.py
+++ seek.py
@@ -1,6 +1,11 @@
-with open('file.txt','r') as f :
-    print(type(f))
+# with open('file.txt','r') as f :
+#     print(type(f))
 
-    f.seek(10)
-    data=f.read(5)
-    print(data)
+#     f.seek(10)
+#     data=f.read(5)
+#     print(data)
+with open('sample.txt','w') as f:
+    f.write('Hello world')
+    f.truncate(5)
+with open('sample.txt','r') as f:
+    print(f.read())
