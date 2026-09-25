e5c05303b2edc6a641bd17dc488935730b07ba9b
amith-67968 <amithbhat128@gmail.com>
2026-09-26 00:47:39 +0530

    Add programmer class and demonstrate inheritance with employee

--- inheritance.py
+++ inheritance.py
@@ -4,5 +4,12 @@ class employee:
         self.id=id
     def showdetails(self):
         print(f'The name of the Employee:{self.id} is {self.name}')
-e=employee("Amith",100)
-e.showdetails()
+class programmer(employee):
+    def showlanguage(self):
+        print("the default language is python")
+e1=employee("Amith",100)
+e1.showdetails()
+
+e2=programmer("harry",500)
+e2.showdetails()
+e2.showlanguage()
